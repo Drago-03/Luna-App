639b50dd77514a45a6c92e4cabc72ae27cb90133
Drago <hitmanagent404@gmail.com>
2024-12-25 10:22:14 +0530

    2 build tests passed

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,37 @@
-export default {
+module.exports = {
   plugins: {
+    'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
+    'postcss-preset-env': {
+      stage: 1,
+      features: {
+        'nesting-rules': true,
+        'custom-properties': true,
+        'custom-media-queries': true,
+        'custom-selectors': true,
+      },
+    },
+    'cssnano': process.env.NODE_ENV === 'production' ? {
+      preset: ['default', {
+        discardComments: {
+          removeAll: true,
+        },
+        normalizeWhitespace: false,
+        colormin: true,
+        minifyFontValues: true,
+        minifyGradients: true,
+      }]
+    } : false,
+    'postcss-import': {},
+    'postcss-flexbugs-fixes': {},
+    'postcss-normalize': {},
   },
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      './src/**/*.{js,jsx,ts,tsx}',
+      './public/index.html'
+    ]
+  }
 };
